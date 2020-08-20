9ee264d34d98c90ce695d801fec3ff9bf491cf03
Yudu Ban <banyudu@gmail.com>
2020-08-21 06:29:37 +0800

    feat: add isAst methods in utils

---
+++ src/scripts/add-is-ast-methods-in-utils.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env ts-node
+
+/**
+//  * Add is_ast_xxx functions in lib/utils.ts
+ */
+
+import { getProject } from '../utils'
+// import { Node } from 'ts-morph'
+
+const camelToSnakeCase = (str: string): string => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
+
+;
+(async () => {
+  const project = getProject()
+  const utilsFile = project.getSourceFile(file => file.getFilePath().includes('lib/utils.ts'))
+  const astIndexFile = project.getSourceFile(file => file.getFilePath().includes('lib/ast/index.ts'))
+  const astClasses = astIndexFile?.getExportSymbols() ?? []
+
+  const astImportDeclaration = (utilsFile?.getImportDeclarations().filter(
+    item => item.getModuleSpecifierValue().endsWith('/ast')
+  ) ?? [])[0]
+  const astNameSet = new Set(astImportDeclaration.getNamedImports().map(e => e.getName()))
+
+  for (const item of astClasses) {
+    const name = item.getName()
+    if (name.startsWith('AST_')) {
+      const funcName = 'is_ast' + camelToSnakeCase(name.substr('AST_'.length))
+      utilsFile?.addStatements(`
+export function ${funcName} (node: AST_Node): node is ${name} {
+  return node?.isAst?.('${name}')
+}
+      `)
+
+      if (!astNameSet.has(name)) {
+        astImportDeclaration.addNamedImport(name)
+      }
+    }
+  }
+  // console.log(utilsFile?.getImportDeclarations().map(item => item.getNamedImports().map(e => e.getName())))
+  await project.save()
+})().catch(console.error)
